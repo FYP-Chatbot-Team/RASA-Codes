093c5108b5d06fc23ca6d85db6f10ca087a11dee
syalabil <syabil125@gmail.com>
2022-01-23 12:19:38 +0800

    Update: Update Rasa Chatbot - Syabil

--- start_services.sh
+++ start_services.sh
@@ -1,4 +1,15 @@
-cd app/
+#cd app/
 # Start rasa server with nlu model
-rasa run --model models --enable-api --cors "*" --debug \
-         -p $PORT
+#rasa run --model models --enable-api --cors "*" --debug \
+#         -p $PORT
+
+#!/bin/sh
+
+if [ -z "$PORT"]
+then
+  PORT=5005
+fi
+
+rasa run --enable-api --port $PORT
+
+rasa run actions
